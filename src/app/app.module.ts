374426b88fd3f86820373d1cce03a2ba3644cf26
CodeTile <43536260+CodeTile@users.noreply.github.com>
2022-12-07 09:43:47 +0000

    inital chips component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { AngularDetailsComponent } from './angular-details.component';
-import {MaterialModule} from './material-module';
-
+import { MaterialModule } from './material-module';
+import { ChipsComponent } from './controls/chips/chips.component';
 @NgModule({
-  imports:      [ BrowserModule, FormsModule , MaterialModule],
-  declarations: [ AppComponent, AngularDetailsComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule,
+    FormsModule,
+    MaterialModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+  ],
+  declarations: [AppComponent, AngularDetailsComponent, ChipsComponent],
+  bootstrap: [AppComponent],
+  entryComponents: [ChipsComponent],
 })
-export class AppModule { }
+export class AppModule {}
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
